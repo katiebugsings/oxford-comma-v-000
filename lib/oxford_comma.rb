974fbbe33d61e340f1bfd8623fae903540bfadcb
Katie Cassell <kcassell944@gmail.com>
2018-02-15 18:04:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
-if array.count == 2
+   if array.count == 1
+     array []
+elsif array.count == 2
   array.join(" and ")
+elsif array.count == 3
+  array.join(", and ")
 end
 end
